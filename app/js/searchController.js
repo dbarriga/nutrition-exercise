422f87ff1928d46a532557dd0e2a36ede6fcf20b
David Brriga <dbarriga@itexico.net>
2016-07-13 18:37:50 -0500

    favorites add/remove

--- app/js/searchController.js
+++ app/js/searchController.js
@@ -6,7 +6,6 @@ angular.module('myApp').controller('searchCtrl', ['$scope', 'searchService', '$r
             searchService.getFood($scope.food).then(function(result)
             {
                 $scope.foodResult = result.data;
-                console.log($scope.foodResult);
             });
         };
 
@@ -16,14 +15,41 @@ angular.module('myApp').controller('searchCtrl', ['$scope', 'searchService', '$r
                 let x = localStorage.favorites.length > 0 ? localStorage.favorites.split(',') : [];
                 x.push(food.ndbno);
                 localStorage.favorites = x.toString();
+                $scope.favorites.push(food);
             }
             else
             {
-                let x = localStorage.favorites.split(',');
-                let index = x.indexOf(food.ndbno);
-                x.splice(index, 1);
-                localStorage.favorites = x.toString();
+                $scope.removeFavorite(food);
             }
         }
+
+        $scope.removeFavorite = function(food)
+        {
+            let x = localStorage.favorites.split(',');
+            let index = x.indexOf(food.ndbno);
+            x.splice(index, 1);
+            localStorage.favorites = x.toString();
+            $scope.favorites.forEach(function(item){
+                if (item.ndbno === food.ndbno) {
+                    delete item;
+                }
+            });
+            $scope.favorites.splice(index)
+        };
+
+        function checkFavorites()
+        {
+            $scope.favorites = [];
+            let x = localStorage.favorites.length > 0 ? localStorage.favorites.split(',') : [];
+            x.forEach(function(item)
+            {
+                searchService.getFood(item).then(function(result)
+                {
+                    $scope.favorites.push(result.data.list.item[0]);
+                });
+            });
+        }
+
+        checkFavorites();
 
 }]);
